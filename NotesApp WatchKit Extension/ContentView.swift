7a623e8cd4eb79d676d042846b246a6ed8807421
Ian Pedraza <ianpedrazacelon@gmail.com>
2022-01-31 16:57:08 -0600

    Initial Commit

---
+++ NotesApp WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NotesApp WatchKit Extension
+//
+//  Created by Ian Pedraza on 31/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NotesApp WatchKit Extension/NotesAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesAppApp.swift
+//  NotesApp WatchKit Extension
+//
+//  Created by Ian Pedraza on 31/01/22.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
